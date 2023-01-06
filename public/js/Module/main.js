a6dcf1e988e78853d45dee2833796218aaa0dbab
VitorLucas44 <vitoriosolucas@hotmail.com>
2023-01-06 14:03:21 +0100

    lesBoss

--- public/js/Module/main.js
+++ public/js/Module/main.js
@@ -2,3 +2,10 @@ import {Boss} from './boss';
 import {Warrior} from './classe';
 import {Mage} from './classe';
 import {Archer} from './classe';
+
+ // Liste des boss disponibles
+ const bosses = [
+    new Boss("Sauron", 200, 30),
+    new Boss("Chronos", 250, 25),
+    new Boss("Lilith", 300, 20)
+    ];
