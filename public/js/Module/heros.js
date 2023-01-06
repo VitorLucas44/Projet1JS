19c01553b580d2dcebb5e7b04e46c8f2aa037f7d
VitorLucas44 <vitoriosolucas@hotmail.com>
2023-01-06 13:42:40 +0100

    boss

---
+++ public/js/Module/heros.js
@@ -0,0 +1,10 @@
+class Hero{
+    constructor(self, name, health, attack, defense){
+    this.name = name
+    this.health = health
+    this.attack = attack
+    this.defense = defense
+    this.actions = ["attaque", "defense"]
+    
+    }
+}
